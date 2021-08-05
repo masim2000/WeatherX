072a3d05e6e8d9923302141d38ca22cabc9786f0
Asim <asim@brandver.se>
2021-08-05 15:35:10 +0500

    Refactored

--- lib/services/dynamic_link_api.dart
+++ lib/services/dynamic_link_api.dart
@@ -14,29 +14,6 @@ class DynamicLinksApi {
     });
   }
 
-  Future<String> createReferralLink(String referralCode) async {
-    final DynamicLinkParameters dynamicLinkParameters = DynamicLinkParameters(
-      uriPrefix: 'https://heloo.page.link',
-      link: Uri.parse('https://asim123.com/refer?code=ref-12345'),
-      androidParameters: AndroidParameters(
-        packageName: 'com.brandverse.weatherx',
-      ),
-      socialMetaTagParameters: SocialMetaTagParameters(
-        title: 'Refer A Friend',
-        description: 'Refer and earn',
-        imageUrl: Uri.parse(
-            'https://www.insperity.com/wp-content/uploads/Referral-_Program1200x600.png'),
-      ),
-    );
-
-    final ShortDynamicLink shortLink =
-        await dynamicLinkParameters.buildShortLink();
-
-    final Uri dynamicUrl = shortLink.shortUrl;
-    print(dynamicUrl);
-    return dynamicUrl.toString();
-  }
-
   void handleSuccessLinking(PendingDynamicLinkData data) {
     final Uri deepLink = data?.link;
 
@@ -44,7 +21,6 @@ class DynamicLinksApi {
       var isRefer = deepLink.pathSegments.contains('refer');
       if (isRefer) {
         var code = deepLink.queryParameters['code'];
-        print(code.toString());
         if (code != null) {
           GeneratedRoute.navigateTo(
             HomeScreen.routeName,
